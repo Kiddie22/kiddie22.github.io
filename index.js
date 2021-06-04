1427fabe30856509f76e9748959789375b126fd9
Kiddie22 <rasula.20191118@iit.ac.lk>
2021-06-04 09:06:40 +0530

    added CV

--- index.js
+++ index.js
@@ -3,36 +3,35 @@
  ---------------------------------------- */
 
 const handleFirstTab = (e) => {
-  if(e.key === 'Tab') {
-    document.body.classList.add('user-is-tabbing')
+  if (e.key === 'Tab') {
+    document.body.classList.add('user-is-tabbing');
 
-    window.removeEventListener('keydown', handleFirstTab)
-    window.addEventListener('mousedown', handleMouseDownOnce)
+    window.removeEventListener('keydown', handleFirstTab);
+    window.addEventListener('mousedown', handleMouseDownOnce);
   }
-
-}
+};
 
 const handleMouseDownOnce = () => {
-  document.body.classList.remove('user-is-tabbing')
+  document.body.classList.remove('user-is-tabbing');
 
-  window.removeEventListener('mousedown', handleMouseDownOnce)
-  window.addEventListener('keydown', handleFirstTab)
-}
+  window.removeEventListener('mousedown', handleMouseDownOnce);
+  window.addEventListener('keydown', handleFirstTab);
+};
 
-window.addEventListener('keydown', handleFirstTab)
+window.addEventListener('keydown', handleFirstTab);
 
-const backToTopButton = document.querySelector(".back-to-top");
+const backToTopButton = document.querySelector('.back-to-top');
 let isBackToTopRendered = false;
 
 let alterStyles = (isBackToTopRendered) => {
-  backToTopButton.style.visibility = isBackToTopRendered ? "visible" : "hidden";
+  backToTopButton.style.visibility = isBackToTopRendered ? 'visible' : 'hidden';
   backToTopButton.style.opacity = isBackToTopRendered ? 1 : 0;
   backToTopButton.style.transform = isBackToTopRendered
-    ? "scale(1)"
-    : "scale(0)";
+    ? 'scale(1)'
+    : 'scale(0)';
 };
 
-window.addEventListener("scroll", () => {
+window.addEventListener('scroll', () => {
   if (window.scrollY > 700) {
     isBackToTopRendered = true;
     alterStyles(isBackToTopRendered);
@@ -41,3 +40,29 @@ window.addEventListener("scroll", () => {
     alterStyles(isBackToTopRendered);
   }
 });
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById('myBtn');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName('close')[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = 'block';
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = 'none';
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
